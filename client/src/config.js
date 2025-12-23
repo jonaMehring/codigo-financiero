29b6234a2e2013866eb6299c9c859e16bef04b0c
jonaMehring <jonatanmehring9@gmail.com>
2025-12-23 19:05:52 -0300

    asd

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,5 @@
 // client/src/config.js
-
-// URL pública de tu API en Koyeb (ej: https://tu-app.koyeb.app)
+  
 export const API_URL = import.meta.env.VITE_API_URL || "https://primary-violetta-asesoriatecnologica-05a5ba92.koyeb.app/";
 
 // PDF servido por el mismo front (Vite) desde /public/ebook/...
